9dd7baba656edf032572e4024a8ed007c7a86d31
mikeygrety <mikeygrety@yahoo.com>
2026-09-16 19:57:18 -0400

    Update AVX512HookTemplateGenerator.h

--- x/AVX512HookTemplateGenerator.h
+++ x/AVX512HookTemplateGenerator.h
@@ -1,28 +1,17 @@
-//
-//  AVX512HookTemplateGenerator.h
-//  AVX512 by DELvEK.NET
-//
-//  Runtime recon -> class selection -> method inspection ->
-//  project generation.
-//
-//  Generates a self-contained dylib project containing:
-//
-//      AVX512Hook.h
-//      AVX512Hook.m
-//      README.md
-//      build.sh
-//      OR
-//      .github/workflows/build.yml
-//
-//  The generated implementation is a diagnostic/test harness.
-//  Method metadata is discovered from the live Objective-C runtime.
-//
+// x/AVX512HookTemplateGenerator.h
 
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface AVX512HookTemplateGenerator : UITableViewController
+@end
+
+@interface AVX512InspectorDataSource : NSObject <UITableViewDataSource>
+- (instancetype)initWithLines:(NSArray<NSString *> *)lines
+                     className:(NSString *)className
+              selectionHandler:(void (^)(void))selectionHandler;
+- (void)selectClass;
 @end
 
 NS_ASSUME_NONNULL_END
