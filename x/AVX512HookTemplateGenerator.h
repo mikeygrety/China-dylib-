472a468ded2d6427d7ea90f2d3829e22795f3811
mikeygrety <mikeygrety@yahoo.com>
2026-09-17 12:45:07 -0400

    Update AVX512HookTemplateGenerator.h

--- x/AVX512HookTemplateGenerator.h
+++ x/AVX512HookTemplateGenerator.h
@@ -1,11 +1,17 @@
 //
+
 //  AVX512HookTemplateGenerator.h
+
 //  AVX512HookTemplateGenerator
+
 //
+
 //  AVX512 by DELvEK.NET
+
 //
 
 #import <UIKit/UIKit.h>
+
 #import <objc/runtime.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -13,13 +19,17 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - Dylib Configuration
 
 typedef NS_ENUM(NSInteger, AVX512DylibType) {
-    AVX512DylibTypeDiagnostic = 0,
-    AVX512DylibTypeTestHarness
+
+AVX512DylibTypeDiagnostic = 0,
+AVX512DylibTypeTestHarness
+
 };
 
 typedef NS_ENUM(NSInteger, AVX512BuildStyle) {
-    AVX512BuildStyleShell = 0,
-    AVX512BuildStyleGitHubActions
+
+AVX512BuildStyleShell = 0,
+AVX512BuildStyleGitHubActions
+
 };
 
 #pragma mark - Method Information
@@ -27,15 +37,21 @@ typedef NS_ENUM(NSInteger, AVX512BuildStyle) {
 @interface AVX512MethodInfo : NSObject
 
 @property (nonatomic, copy) NSString *selectorName;
+
 @property (nonatomic, copy) NSString *typeEncoding;
+
 @property (nonatomic, copy) NSString *returnType;
 
 @property (nonatomic, assign) NSUInteger argumentCount;
+
 @property (nonatomic, assign) BOOL inherited;
 
 @property (nonatomic, assign) IMP implementation;
-@property (nonatomic, copy) NSString *imageName;
+
+@property (nonatomic, copy, nullable) NSString *imageName;
+
 @property (nonatomic, assign) uintptr_t implementationAddress;
+
 @property (nonatomic, assign) uintptr_t imageOffset;
 
 @end
@@ -45,8 +61,10 @@ typedef NS_ENUM(NSInteger, AVX512BuildStyle) {
 @interface AVX512ClassInfo : NSObject
 
 @property (nonatomic, assign) Class cls;
+
 @property (nonatomic, copy) NSString *className;
-@property (nonatomic, copy) NSString *superclassName;
+
+@property (nonatomic, copy, nullable) NSString *superclassName;
 
 @property (nonatomic, strong) NSArray<AVX512MethodInfo *> *methods;
 
@@ -57,16 +75,21 @@ typedef NS_ENUM(NSInteger, AVX512BuildStyle) {
 @interface AVX512InstructionInfo : NSObject
 
 @property (nonatomic, assign) uintptr_t address;
+
 @property (nonatomic, assign) uint32_t instructionWord;
 
 @property (nonatomic, copy) NSString *mnemonic;
+
 @property (nonatomic, copy) NSString *operands;
+
 @property (nonatomic, copy) NSString *bytes;
 
 @property (nonatomic, copy) NSString *explanation;
 
 @property (nonatomic, assign) BOOL isBranch;
+
 @property (nonatomic, assign) BOOL isCall;
+
 @property (nonatomic, assign) BOOL isReturn;
 
 @property (nonatomic, assign) BOOL canNOP;
@@ -77,16 +100,20 @@ typedef NS_ENUM(NSInteger, AVX512BuildStyle) {
 
 @interface AVX512PatchInfo : NSObject
 
-@property (nonatomic, copy) NSString *imageName;
+@property (nonatomic, copy, nullable) NSString *imageName;
+
 @property (nonatomic, copy) NSString *className;
+
 @property (nonatomic, copy) NSString *selectorName;
 
 @property (nonatomic, assign) uintptr_t address;
 
 @property (nonatomic, copy) NSString *originalInstruction;
+
 @property (nonatomic, copy) NSString *replacementInstruction;
 
 @property (nonatomic, copy) NSString *originalBytes;
+
 @property (nonatomic, copy) NSString *replacementBytes;
 
 @property (nonatomic, assign) BOOL isNOP;
