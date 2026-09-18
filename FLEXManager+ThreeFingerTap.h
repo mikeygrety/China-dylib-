909d28c1e8d25c40114d4effbdd13f7347a45f50
mikeygrety <mikeygrety@yahoo.com>
2026-09-18 02:19:01 -0400

    Update FLEXManager+ThreeFingerTap.h

--- FLEXManager+ThreeFingerTap.h
+++ FLEXManager+ThreeFingerTap.h
@@ -1,9 +1,18 @@
-#import “AVX512Manager.h”
-
-NS_ASSUME_NONNULL_BEGIN
-
+//
+//  FLEXManager+ThreeFingerTap.h
+//  AVX512
+//
+//  Three-finger gesture support for AVX512Manager.
+//
+#import <UIKit/UIKit.h>
+@class AVX512Manager;
 @interface AVX512Manager (ThreeFingerTap)
-
+/// Installs or updates the three-finger gesture on the active window.
+- (void)avx512_setupGesture;
+/// Handles UIWindowDidBecomeKeyNotification.
+- (void)avx512_windowDidBecomeKey:(NSNotification *)notification;
+/// Handles UISceneDidActivateNotification.
+- (void)avx512_sceneDidActivate:(NSNotification *)notification;
+/// Finds the current foreground application window.
+- (UIWindow *)avx512_findTargetWindow;
 @end
-
-NS_ASSUME_NONNULL_END
